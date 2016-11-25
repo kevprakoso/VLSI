ac2a4ed931a6a0f25baa6753fb167f9541911eed
Kevin Shidqi <kev.prakoso@yahoo.com>
2016-11-25 17:19:53 +0700

    Create clockdiv.v

---
+++ EDC/FIFO-fix/clockdiv.v
@@ -0,0 +1,28 @@
+module clockdiv(
+					clk,
+					RST,
+					slow_clk
+    );
+
+input clk, RST;
+output slow_clk;
+
+reg [25:0] count=26'b0;
+reg slow_clk;
+
+
+always @ (posedge clk or posedge RST)
+
+	if (RST==1'b1) begin
+	slow_clk <= 1'b0;
+	count <= 26'b0;
+	end else begin
+		if (count==26'd10000) begin
+			slow_clk <= (~slow_clk);
+			count <= 26'b0;
+		end else begin
+		count <= count +1'b1;
+		end
+	end
+
+endmodule

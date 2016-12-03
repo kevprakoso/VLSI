b13fcf6baa34fd7037867efd466bf71b87b4c0f6
Christoporus Deo Putratama <chris.dp.41@gmail.com>
2016-12-03 11:44:29 +0000

    Add files via upload

---
+++ LSI-Contest/block_diagram/calc.v
@@ -0,0 +1,15 @@
+module  calc (  
+	angle_in,
+	count_n,
+	mag,
+	right_p,
+	left_p
+	);
+	
+input		angle_in;
+input		count_n;
+input		mag;
+output	right_p;
+output	left_p;
+
+endmodule
---
+++ LSI-Contest/block_diagram/histogram.v
@@ -0,0 +1,13 @@
+module histogram (  
+	count_n,
+	right_p,
+	left_p,
+	output_h
+	);
+	
+input		count_n;
+input	right_p;
+input	left_p;
+output output_h;
+
+endmodule

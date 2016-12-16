65c17238276af4a1e46c04ffca25b70c93a5862f
cputratama <chris.dp.41@gmail.com>
2016-12-16 16:12:23 +0700

    update sample_picture folder added

--- EDC/Gyro.v
+++ EDC/Gyro.v
@@ -1,35 +1,21 @@
-`timescale 1ns/1ns
-module gyro(
-input signed [15:0]	gyroData_x,
-output reg signed [15:0]	x_gyro,
-input wire		 clk,
-input wire		 RST
-);
+module gyro(raw_gyro, raw_data, clk, reset, en);
 
-//reg signed [15:0] x_gyro;
-initial begin
-x_gyro <= 16'd0;
-end
-//assign x_final = x_gyro;
-always @ (posedge clk) begin
+//PORT DECLARATION
+	output [15 : 0 ] raw_gyro;
+	input  [ 15 : 0] raw_data;
+	input clk, reset, en;	
 
-
-	if (gyroData_x[15]==1'b1) begin
-		
-		if (x_gyro >= (x_gyro - {8'd0,gyroData_x[7:0]})) begin
-			 x_gyro <= x_gyro - {8'd0,gyroData_x[7:0]};
-			end
-		else begin
-			 x_gyro <= 16'd360 - ({8'd0,gyroData_x[7:0]} - x_gyro);
-			end
+//REG DECLARATION
 	
-	end else begin
-	if (x_gyro <= 16'd360) begin
-	 x_gyro <= x_gyro + {8'd0,gyroData_x[7:0]};
-	end else begin
-	 x_gyro <= x_gyro - 16'd360;
-	end
-	end
 
-end
+//WIRE DECLARATION
+	wire [15 : 0] acc_out;
+	wire c_out;
+	wire [15 : 0] data_in;
+
+//ALGORITHM
+	add_rca_16	M1(c_out, acc_out,  raw_data, data_in, 1'b0 );
+	flipflop	M2(data_in, raw_gyro, clk, reset, en);
+	flipflop	M3(raw_gyro, acc_out, clk, reset, en);
+
 endmodule

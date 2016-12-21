8cbdc9345eb130c71a4a6ad7e43b68576a6a6669
cputratama <chris.dp.41@gmail.com>
2016-12-21 15:10:14 +0700

    Update read_file 21-12-2016

--- LSI-Contest/MATLAB/img_to_vhdl_txt.m
+++ LSI-Contest/MATLAB/img_to_vhdl_txt.m
@@ -1,9 +1,15 @@
 % Convert an image to a VHDL compatible input file
-function img_to_vhdl_txt(grayscale_img,filename)
+function img_to_vhdl_txt(img,filename)
     % Invert image.
-    grayscale_img = grayscale_img';
+    img_red = (img(:,:,1))';
+    img_green = (img(:,:,2))';
+    img_blue = (img(:,:,3))';    
     % Columnize the image and convert each pixel to a 4 digit hex string
-    hex_stream = cellstr(num2str(grayscale_img(:),'%04x'));
+    hex_stream_red = cellstr(num2str(img_red(:),'%02x'));
+    hex_stream_green = cellstr(num2str(img_green(:),'%02x'));
+    hex_stream_blue = cellstr(num2str(img_blue(:),'%02x'));
+    hex_stream_1 = strcat(hex_stream_red, hex_stream_green);
+    hex_stream_2 = strcat(hex_stream_1, hex_stream_blue);
     % write out file using new line as the delimter
-    dlmwrite(filename, hex_stream, '-append', 'delimiter', '', 'newline', 'pc');
+    dlmwrite(filename, hex_stream_2, '-append', 'delimiter', '', 'newline', 'pc');
 end
--- LSI-Contest/MATLAB/test.m
+++ LSI-Contest/MATLAB/test.m
@@ -1,4 +1,3 @@
-imageName = 'test_pos_1.png';
-img = imread(imageName);
-img1 = greyscale(img);
-img_to_vhdl_txt(img1,'test_pos_1.raw');
+imageName = 'test_pos_3.png';
+img = double(imread(imageName));
+img_to_vhdl_txt(img,'test_pos_3.raw');
